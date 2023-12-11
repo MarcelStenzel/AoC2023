55e17bb95ce3cb87c0812ef3f30c2f279e4c3b22
Marcel Stenzel <marcel.stenzel@haw-hamburg.de>
2023-12-11 19:07:39 +0100

    Add working solution for Day 3 part 1

--- AoC2023/Day3.cs
+++ AoC2023/Day3.cs
@@ -12,37 +12,26 @@ class Day3 : AdventDays
         StreamReader reader = new(@"..\..\..\input\Day3.txt");
         currentLine = reader.ReadLine()!;
         nextLine = reader.ReadLine()!;
+        int sum = 0;
 
         do {
             for (int i = 0; i < currentLine.Length; i++) {
                 int numLen = 0;
-                int number = currentLine[i] >= '0' && currentLine[i] <= '9' ? CheckNum(i, out numLen) : -1;  //(int)char.GetNumericValue(currentLine[i]);
-                if (number < 0) {
-                    if (numLen > 0) {
-                        i += numLen - 1;
-                    }
-                    continue;
+                int number = currentLine[i] >= '0' && currentLine[i] <= '9' ? CheckNum(i, out numLen) : -1;
+                if (number > 0) {
+                    Console.WriteLine($"number: {number}");
+                    sum += number;
                 }
-                // if (number >= 0) {
-                //     currentNum *= 10;
-                //     currentNum += digit;
-
-                //     //Check if it a number that counts
-
-
-                // } else {
-                //     if (currentNum > 0) {
-                //         //Last digit was the last of the current number
-                //     }
-                //     Console.WriteLine(currentNum);
-                //     currentNum = 0;
-                // }
+                if (numLen > 0) {
+                    i += numLen - 1;
+                }
             }
 
             beforeLine = currentLine;
             currentLine = nextLine;
             nextLine = reader.ReadLine()!;
-        } while (nextLine != null);
+        } while (currentLine != null);
+        Console.WriteLine(sum);
     }
 
     private int CheckNum(int index, out int numLen) {
@@ -51,18 +40,55 @@ class Day3 : AdventDays
         }
 
         bool validNum = false;
-        bool firstDigit = true;
-        int sum = (int)char.GetNumericValue(currentLine[index]);
+        int num = (int)char.GetNumericValue(currentLine[index]);
         numLen = 1;
 
+        /* Check column before first digit */
         if (index != 0) {
-            validNum = IsSymbol(currentLine[index - 1]);
+            validNum = CheckColumn(index - 1);
         }
 
+        int iterator = 0; 
+        bool isDigit = true;
+        int digit;
 
+        do {
+            if (!validNum) {
+                /* Check above the digit */
+                if (!string.IsNullOrEmpty(beforeLine) && IsSymbol(beforeLine[index + iterator])) {
+                    validNum = true;
+                }
 
+                /* Check below the digit */
+                if (!string.IsNullOrEmpty(nextLine) && IsSymbol(nextLine[index + iterator])) {
+                    validNum = true;
+                }
+            }
 
-        return validNum ? sum : -1;
+            iterator++;
+            if (index  + iterator < currentLine.Length && (digit = (int)char.GetNumericValue(currentLine[index + iterator])) >= 0) {
+                num *= 10;
+                num += digit;
+                numLen++;
+            } else {
+                isDigit = false;
+            }
+
+        } while (isDigit);
+
+        /* Check column after last digit */
+        if (!validNum && index + iterator < currentLine.Length) {
+            validNum = CheckColumn(index + iterator);
+        }
+
+        return validNum ? num : -1;
+    }
+
+    private bool CheckColumn(int index) {
+        if (!string.IsNullOrEmpty(beforeLine) && IsSymbol(beforeLine[index])) return true;
+        if (!string.IsNullOrEmpty(currentLine) && IsSymbol(currentLine[index])) return true;
+        if (!string.IsNullOrEmpty(nextLine) && IsSymbol(nextLine[index])) return true;
+        return false;
     }
 
     private static bool IsSymbol(char c) {
